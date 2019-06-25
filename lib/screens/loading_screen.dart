6b9eea404b3011063af0f85e3610ceede3da9076
nikhil914 <nikhilranjan914@gmail.com>
2019-06-25 06:33:25 +0530

    Working on real Data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,29 +3,34 @@ import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+const Apikey = '44465be905ef7d408988716706c152fe';
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
+
+double langt;
+double longt;
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
     getlocation();
-    getData();
   }
 
   void getlocation() async {
     Location loc = Location();
     await loc.getCurrentLocation();
-    print(loc.latitude);
-    print(loc.longitude);
+    langt = loc.latitude;
+    longt = loc.longitude;
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$langt&lon=$longt&appid=$Apikey');
     if (response.statusCode == 200) {
       String data = response.body;
 
